5a4cc18e520a728f8a944b9614d2405baae8ed8f
svetlanagaydey <lana84@ukr.net>
2021-11-07 12:21:15 +0200

    js 1.1 completed

---
+++ JavaStript/index.js
@@ -0,0 +1,23 @@
+use 'use strick';
+let stringVal = 'Hello friends!';
+let numberVal = 22;
+let objectVal = {
+    name:'John',
+    age:18
+}
+let booleanVal = true;
+var nullVal = null;
+let underfinedVal;
+let symbolVal = Symbol("symbol");
+
+
+
+
+console.log(typeof(stringVal)); 
+console.log(typeof(numberVal)); 
+console.log(typeof(objectVal)); 
+console.log(typeof(booleanVal)); 
+console.log(typeof(nullVal));  // console object, mistake JavaScript!)))
+console.log(typeof(undefinedVal)); 
+console.log(typeof(symbolVal));
+
