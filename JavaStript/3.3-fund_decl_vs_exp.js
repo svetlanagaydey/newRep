218c3dabd1735424271b71729a45f9bb08951468
svetlanagaydey <lana84@ukr.net>
2021-11-08 12:51:43 +0200

    completed

---
+++ JavaStript/3.3-fund_decl_vs_exp.js
@@ -0,0 +1,20 @@
+const WORLD_POPULATION = 7900;
+let populationChina = 1441;
+let populationUsa = 339.5;
+let populationUkraine = 45;
+
+function percentageOfWorld1 (population) {
+    return (population / WORLD_POPULATION * 100).toFixed(1);
+}
+console.log(percentageOfWorld1(populationChina));  // for China
+console.log(percentageOfWorld1(populationUsa));  // for USA
+console.log(percentageOfWorld1(populationUkraine));  // for Ukraine
+
+const percentageOfWorld2 = population => (population/WORLD_POPULATION*100).toFixed(2);
+
+console.log(percentageOfWorld2(populationChina));
+console.log(percentageOfWorld2(populationUsa));
+console.log(percentageOfWorld2(populationUkraine));
+
+
+
