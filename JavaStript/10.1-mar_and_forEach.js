a94329d8e3f82e5a5f489523db64f96a3ba716cc
svetlanagaydey <lana84@ukr.net>
2021-11-15 13:16:25 +0200

    completed

---
+++ JavaStript/10.1-mar_and_forEach.js
@@ -0,0 +1,46 @@
+const newReleases = [ {
+    id: 70111470,
+    title: "Die Hard",
+    boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg", uri: "http://api.netflix.com/catalog/titles/movies/70111470", rating: [4.0],
+    bookmark: [],
+    }, {
+    id: 654356453,
+    title: "Bad Boys",
+    boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg", uri: "http://api.netflix.com/catalog/titles/movies/70111470", rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+    }, {
+    id: 65432445,
+    title: "The Chamber",
+    boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg", uri: "http://api.netflix.com/catalog/titles/movies/70111470", rating: [4.0],
+    bookmark: [],
+    }, {
+    id: 675465,
+    title: "Fracture",
+    boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg", uri: "http://api.netflix.com/catalog/titles/movies/70111470", rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+    }, ];
+
+
+function forEachFunction(array) {
+    let newArray = [];
+    array.forEach(function (el) {
+        let newObject = {};
+        newObject.id = el.id;
+        newObject.title = el.title;
+        newArray.push(newObject);
+    });
+    return newArray;
+}
+
+
+function mapFunction(array) {
+    return array.map(function(element) {
+       return{
+           id: element.id,
+           title: element.title
+       }
+    });
+}
+
+console.log(forEachFunction(newReleases));
+console.log(mapFunction(newReleases));
