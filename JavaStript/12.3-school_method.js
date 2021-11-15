352a3c73c55c3bd1d0b09f27acbbcd53ae8cef3e
svetlanagaydey <lana84@ukr.net>
2021-11-15 22:27:17 +0200

    first part completed

---
+++ JavaStript/12.3-school_method.js
@@ -0,0 +1,50 @@
+const school = {
+    teachers:
+    [
+        {
+            id: 1,
+            name: "Pinchas",
+            subjects: ["chemistry", "biology", "physics"],
+            students: [],
+            capacityLeft: 3,
+        },
+        {
+            id: 2,
+            name: "Williams",
+            subjects: ["history", "ethics"],
+            students: [],
+            capacityLeft: 2,
+        },
+    ],  
+    students: [
+        {
+            id: 10,
+            name: "Jennifer",
+            age: 20,
+        },
+        {
+            id: 11,
+            name: "Howard",
+            age: 23,
+        },
+        {
+            id: 12,
+            name: "Old-Timmy",
+            age: 86,
+        },
+        {
+            id:13,
+            name: "Houston",
+            age: 21,
+        },
+    ],
+    findPerson(type, id) {
+        if (type == 'teachers') {
+            return this.teachers.find(teacher => teacher.id === id);
+        }
+        if (type == 'students') {
+            return this.students.find(studend => studend.id === id);
+        }
+    }
+};
+console.log(school.findPerson('students', 13));
