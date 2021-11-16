923986f2a11acac6967fdcc3782e7910cf12f9db
svetlanagaydey <lana84@ukr.net>
2021-11-16 12:59:59 +0200

    completed

---
+++ JavaStript/13.2-sort_strings.js
@@ -0,0 +1,40 @@
+const foods = ["alafel", "abich", "3ummus", "2izza with extra pineapple"];
+const words = ["apple", "supercalifragilisticexpialidocious", "hi", "zoo"];
+const foodsWithUpperCase = [ "falafel", "Sabich", "hummus", "pizza with extra pineapple", ];
+
+function ascOrder(array) {
+    return (array.sort((a,b) => {
+        return a > b ? 1 : a == 0 ? 0 : -1;
+    }));
+}
+function descOrder(array) {
+    return (array.sort((a,b) => {
+        return a < b ? 1 : a == 0 ? 0 : -1;
+    }));
+}
+function ascUpperOrder(array) {
+    return (array.sort((a,b) => {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        return a < b ? 1 : (a == 0) ? 0 : -1;
+    }));
+}
+function decsUpperOrder(array) {
+    return (array.sort((a,b) => {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        return a > b ? 1 : (a == 0) ? 0 : -1;
+    }));
+}
+
+function longestWord(array) {
+    return array.sort((a,b) => a.length - b.length);
+}
+
+
+console.log(words);
+console.log(longestWord(words));
+console.log(ascOrder(foods));
+console.log(descOrder(foods));
+console.log(ascUpperOrder(foodsWithUpperCase));
+console.log(decsUpperOrder(foodsWithUpperCase));
