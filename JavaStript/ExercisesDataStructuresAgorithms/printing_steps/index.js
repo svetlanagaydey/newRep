a9cf580f2ad140acfd4baf5db82e4ef57969692e
svetlanagaydey <lana84@ukr.net>
2022-02-16 07:59:56 +0200

    printing steps exercise complited

---
+++ JavaStript/ExercisesDataStructuresAgorithms/printing_steps/index.js
@@ -0,0 +1,34 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n) {
+    for (let i=1; i <= n; i++) {
+        let result = '';
+        for (let j=1; j <= n; j++) {
+            if (j <= i) {
+                result += '#'
+            } else {
+                result += ' '
+            }
+        }
+        console.log(result);
+    }
+}
+steps(5);
+module.exports = steps;
