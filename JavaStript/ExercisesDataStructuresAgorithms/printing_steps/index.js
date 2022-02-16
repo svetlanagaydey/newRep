db0be7b93b8461376570a836f7c00da4b363e89a
svetlanagaydey <lana84@ukr.net>
2022-02-16 10:19:04 +0200

    printing step exercise rucursive solition

--- JavaStript/ExercisesDataStructuresAgorithms/printing_steps/index.js
+++ JavaStript/ExercisesDataStructuresAgorithms/printing_steps/index.js
@@ -17,18 +17,34 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    for (let i=1; i <= n; i++) {
-        let result = '';
-        for (let j=1; j <= n; j++) {
-            if (j <= i) {
-                result += '#'
-            } else {
-                result += ' '
-            }
-        }
-        console.log(result);
+// function steps(n) {
+//     for (let i=1; i <= n; i++) {
+//         let result = '';
+//         for (let j=1; j <= n; j++) {
+//             if (j <= i) {
+//                 result += '#'
+//             } else {
+//                 result += ' '
+//             }
+//         }
+//         console.log(result);
+//     }
+// }
+
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
     }
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row+1);
+    }
+    if (stair.length <= row) {
+        stair += "#";
+    } else {
+         stair += "-";
+    }
+    steps(n, row, stair);
 }
 steps(5);
 module.exports = steps;
