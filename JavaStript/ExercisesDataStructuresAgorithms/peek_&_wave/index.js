a4f3b10b295ae852cb1683f989fbeebb6667c1a2
svetlanagaydey <lana84@ukr.net>
2022-02-21 17:00:15 +0200

    weave exercise enother solution

--- JavaStript/ExercisesDataStructuresAgorithms/peek_&_wave/index.js
+++ JavaStript/ExercisesDataStructuresAgorithms/peek_&_wave/index.js
@@ -1,3 +1,27 @@
+// --- Directions
+// 1) Complete the task in weave/queue.js
+// 2) Implement the 'weave' function.  Weave
+// receives two queues as arguments and combines the
+// contents of each into a new, third queue.
+// The third queue should contain the *alterating* content
+// of the two queues.  The function should handle
+// queues of different lengths without inserting
+// 'undefined' into the new one.
+// *Do not* access the array inside of any queue, only
+// use the 'add', 'remove', and 'peek' functions.
+// --- Example
+//    const queueOne = new Queue();
+//    queueOne.add(1);
+//    queueOne.add(2);
+//    const queueTwo = new Queue();
+//    queueTwo.add('Hi');
+//    queueTwo.add('There');
+//    const q = weave(queueOne, queueTwo);
+//    q.remove() // 1
+//    q.remove() // 'Hi'
+//    q.remove() // 2
+//    q.remove() // 'There'
+
 class Queue {
     constructor() {
       this.data = [];
@@ -17,21 +41,30 @@ class Queue {
   }
 
   function weave(sourceOne, sourceTwo) {
-    let result = new Queue();
-    console.log(result.data)
-    for (let i=sourceOne.data.length - 1; i >= 0; i--) {
-        result.add(sourceOne.peek());
-        sourceOne.remove();
-        if (sourceTwo.peek()) {
-            result.add(sourceTwo.peek());
-            sourceTwo.remove();
-        }   
+    const result = new Queue();
+    console.log(result.data);
+
+    while (sourceOne.peek() || sourceTwo.peek()) {
+      if (sourceOne.peek()) {
+        result.add(sourceOne.remove());
+      }
+      if (sourceTwo.peek()) {
+        result.add(sourceTwo.remove());
+      }
     }
-    if (sourceOne.data.length === 0 && sourceTwo.data.length !==0) {
-        sourceTwo.data.forEach(element => {
-            result.add(element);
-        });
-    }
+    // for (let i=sourceOne.data.length - 1; i >= 0; i--) {
+    //     result.add(sourceOne.peek());
+    //     sourceOne.remove();
+    //     if (sourceTwo.peek()) {
+    //         result.add(sourceTwo.peek());
+    //         sourceTwo.remove();
+    //     }   
+    // }
+    // if (sourceOne.data.length === 0 && sourceTwo.data.length !==0) {
+    //     sourceTwo.data.forEach(element => {
+    //         result.add(element);
+    //     });
+    // }
     return result;
 }
 
