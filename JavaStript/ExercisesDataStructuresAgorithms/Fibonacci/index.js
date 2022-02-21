2d823d75232abf7ca24c0d6c12f2b6c369a5163a
svetlanagaydey <lana84@ukr.net>
2022-02-21 13:03:57 +0200

    fibonacci recursive solution with memoization

--- JavaStript/ExercisesDataStructuresAgorithms/Fibonacci/index.js
+++ JavaStript/ExercisesDataStructuresAgorithms/Fibonacci/index.js
@@ -26,12 +26,36 @@
 //     }
 //     return arrayFibonacci[n]
 
-function fib(n) {
-    if (n < 2) {
-        return n
-    } else {
-        return fib(n-1)+fib(n-2);
-    }
-}
+// function fib(n) {
+//     if (n < 2) {
+//         return n
+//     } else {
+//         return fib(n-1)+fib(n-2);
+//     }
+// }
 
-console.log(fib(2));
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+      if (cache[args]) {
+        return cache[args];
+      }
+  
+      const result = fn.apply(this, args);
+      cache[args] = result;
+  
+      return result;
+    };
+  }
+  
+  function slowFib(n) {
+    if (n < 2) {
+      return n;
+    }
+  
+    return fib(n - 1) + fib(n - 2);
+  }
+  
+  const fib = memoize(slowFib);
+
+console.log(fib(41));
