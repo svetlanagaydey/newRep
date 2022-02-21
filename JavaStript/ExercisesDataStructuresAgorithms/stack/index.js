5fc147b82274185233330a893332608d3b330e71
svetlanagaydey <lana84@ukr.net>
2022-02-21 22:26:28 +0200

    stack exercise my solution

---
+++ JavaStript/ExercisesDataStructuresAgorithms/stack/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Create a stack data structure.  The stack
+// should be a class with methods 'push', 'pop', and
+// 'peek'.  Adding an element to the stack should
+// store it until it is removed.
+// --- Examples
+//   const s = new Stack();
+//   s.push(1);
+//   s.push(2);
+//   s.pop(); // returns 2
+//   s.pop(); // returns 1
+
+class Stack {
+    constructor() {
+        this.data = []
+    }
+    push(string) {
+        this.data.push(string);
+    }
+    pop() {
+        this.data.pop();
+    }
+    peek() {
+        return this.data[this.data.length-1]
+    }
+}
+let stack = new Stack();
+stack.push('hello')
+stack.push('world')
+stack.peek();
+stack.pop();
+
+console.log(stack.data)
