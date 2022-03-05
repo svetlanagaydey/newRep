ab7c62abe43d46b6714576a9ae601dc7f3297cb3
svetlanagaydey <lana84@ukr.net>
2022-03-05 15:51:05 +0200

    all anagrams exercise completed

--- JavaStript/from_codewars/where_my_anagrams_at.js
+++ JavaStript/from_codewars/where_my_anagrams_at.js
@@ -1,24 +1,23 @@
-function anagrams(word, words) {
-  
-    const map = (string) => {
-      const obj = {}
-      for (let chat of string) {
-        obj[chat] = obj[chat] || 1;
-      }
-      console.log(obj)
-    return obj
+// What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+// 'abba' & 'baab' == true
+// 'abba' & 'bbaa' == true
+// 'abba' & 'abbba' == false
+// 'abba' & 'abca' == false
+
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+
+const allAnagrams = (word, array) => {
+    let result = [];
+    for (let el of array) {
+        if (word.split('').sort().join() === el.split('').sort().join())
+        result.push(el);
     }
-  
-    let a = Object.keys(map(word)).sort();
-    let b = Object.keys(map(words)).sort();
-    
-    // if (a === b) return true;
-    // if (a == null || b == null) return false;
-    if (a.length !== b.length) return false;
-  
-    for (var i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
+    return result
 }
-console.log(anagrams("hello", "ehllo"))
+
+console.log(allAnagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']));
