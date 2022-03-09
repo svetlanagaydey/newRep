4d9196058b0fd2fc1a32e0c6d3373d11eee5dfc9
Svetlana Gaydey <svetlana@hbinov.com>
2022-03-09 23:57:29 +0200

    middle point of linked list exercise completed

--- JavaStript/ExercisesDataStructuresAgorithms/middle_point/index.js
+++ JavaStript/ExercisesDataStructuresAgorithms/middle_point/index.js
@@ -6,12 +6,7 @@ const { LinkedList } = require('../linked_list/linked_list')
 // *Do not* use a counter variable, *do not* retrieve
 // the size of the list, and only iterate
 // through the list one time.
-// --- Example
-//   const l = new LinkedList();
-//   l.insertLast('a')
-//   l.insertLast('b')
-//   l.insertLast('c')
-//   midpoint(l); // returns { data: 'b' }
+
 
 function midpoint(list) {}
 
