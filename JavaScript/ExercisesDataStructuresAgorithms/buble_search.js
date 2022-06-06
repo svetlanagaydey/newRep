bc9fa33765c72e2f7684742fd1a74ef3a69004a6
Svetlana Gaydey <lana84@ukr.net>
2022-06-06 19:31:44 +0300

    Bubble Sort algorithm exercise implementation

---
+++ JavaScript/ExercisesDataStructuresAgorithms/buble_search.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Implement bubbleSort
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < (arr.length - i - 1); j++) {
+        if (arr[j] > arr[j+1]) {
+          const lesser = arr[j+1];
+          arr[j+1] = arr[j];
+          arr[j] = lesser;
+        }
+      }
+    }
+      return arr;
+}
+
+console.log(bubbleSort([3,15,-20, 9, -1, 2]))
